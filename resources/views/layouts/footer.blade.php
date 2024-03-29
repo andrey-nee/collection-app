d740137f26612ca336ac72d898347ac38d925db4
andrey-nee <andrey.nee91@gmail.com>
2024-03-29 16:46:06 +0500

    feat: add footer layout

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer footer py-3 my-4 mt-auto bg-body-tertiary">
+    <ul class="footer-nav nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="footer-nav__item nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+      <li class="footer-nav__item nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+    <p class="footer-info text-center text-body-secondary">© 2024 Company, Inc</p>
+  </footer>
