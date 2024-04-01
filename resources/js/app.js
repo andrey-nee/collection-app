55c53931c50b51f89484bcefa9cc3736ce2b5a54
andrey-nee <andrey.nee91@gmail.com>
2024-04-01 09:21:21 +0500

    remove bootstrap.js import

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
-import './bootstrap';
+// AJAX запросы
+// function loadPage(page) {
+//     $.ajax({
+//         url: '/' + page,
+//         type: 'GET',
+//         success: function(responce) {
+//             $('#content').html(responce.content);
+//         },
+//         error: function(xhr, status, error) {
+//             console.error(error);
+//         }
+//     });
+// }
+
+// Site-nav links Events
+// $(document).ready(function(event) {
+//     $('.site-nav__link').click(function(event) {
+//         event.preventDefault();
+//         var page = $(this).attr('data-page');
+//         loadPage(page);
+//     })
+// })
