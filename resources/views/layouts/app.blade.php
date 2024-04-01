58f3da7d7cccc3b9a60a1291e91f26ffdde87092
andrey-nee <andrey.nee91@gmail.com>
2024-04-01 09:24:27 +0500

    feat: update stylesheet and js links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    @vite(['resources/css/bootstrap.css', 'resources/css/styles.css', 'resources/js/app.js'])
+    @vite([
+        'resources/css/bootstrap.min.css',
+        'resources/sass/app.scss',
+        'resources/js/bootstrap.bundle.min.js',
+        'resources/js/app.js'
+    ])
 </head>
 
 <body class="d-flex flex-column h-100">
