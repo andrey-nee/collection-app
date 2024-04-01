b1e41d22077fa9bbf557449b9bbe912a64c6f7ad
andrey-nee <andrey.nee91@gmail.com>
2024-04-01 18:41:28 +0500

    feat: add Film Images model and migration

---
+++ database/migrations/2024_04_01_130607_create_film_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('film_images', function (Blueprint $table) {
+      $table->bigIncrements('id');
+      $table->string('image');
+      $table->bigInteger('film_id')->unsigned();
+
+      // Удаляем строку, если удаляется связанный с ней фильм
+      $table->foreign('film_id')
+        ->references('id')
+        ->on('films')
+        ->onDelete('cascade');
+
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('film_images');
+  }
+};
