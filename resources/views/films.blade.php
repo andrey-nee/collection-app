6e84ca69a515c4131aacb6e8a071591a967d459b
andrey-nee <andrey.nee91@gmail.com>
2024-04-02 16:48:55 +0500

    feat: add table-film classes

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.app')
 
 @section('title')
-Collection | Фильмы
+  Collection | Фильмы
 @endsection
 
 @section('page-title')
-Коллекция фильмов
+  Коллекция фильмов
 @endsection
 
 @section('content')
-
-<div class="table-responsive small py-3">
-    <table class="table table-striped table-sm">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Название</th>
-          <th scope="col">Описание</th>
-          <th scope="col">Жанр</th>
-          <th scope="col">Год</th>
-          <th scope="col">Режиссер</th>
+  <div class="table-responsive small py-3 films">
+    <table class="table table-striped table-sm table-films">
+      <thead class="table-films__head">
+        <tr class="table-films__row">
+          <th scope="col" class="table-films__header">#</th>
+          <th scope="col" class="table-films__header">Обложка</th>
+          <th scope="col" class="table-films__header">Название</th>
+          <th scope="col" class="table-films__header">Описание</th>
+          <th scope="col" class="table-films__header">Жанр</th>
+          <th scope="col" class="table-films__header">Год</th>
+          <th scope="col" class="table-films__header">Режиссер</th>
         </tr>
       </thead>
       <tbody>
+
         @foreach ($films as $film)
           @php
             $image = '';
@@ -35,14 +36,15 @@ Collection | Фильмы
 
           <tr class="table-films__row">
             <td class="table-films__data">{{ $loop->iteration }}</td> {{-- $loop->iteration (встроено в Laravel) считает кол-во итераций внутри @foreach --}}
-            <td><img src="/images/{{ $image }}" alt="{{ $film->name }}"></td>
-          <td>{{ $film->name }}</td>
-          <td>{{ $film->description }}</td>
-          <td>{{ $film->genre_id }}</td>
-          <td>{{ $film->year }}</td>
-          <td>{{ $film->director_id }}</td>
-        </tr>
+            <td class="table-films__data table-films__film-image"><img src="/images/{{ $image }}" alt="{{ $film->name }}"></td>
+            <td class="table-films__data">{{ $film->name }}</td>
+            <td class="table-films__data">{{ $film->description }}</td>
+            <td class="table-films__data">{{ $film->genre_id }}</td>
+            <td class="table-films__data">{{ $film->year }}</td>
+            <td class="table-films__data">{{ $film->director_id }}</td>
+          </tr>
         @endforeach
+
       </tbody>
     </table>
   </div>
