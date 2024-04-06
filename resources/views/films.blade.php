682fa05c39f98108e06f110c7b4a0709590db220
andrey-nee <andrey.nee91@gmail.com>
2024-04-06 13:14:04 +0500

    fix: change modal-title text

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -117,7 +117,7 @@
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="modalFilmInfo">Название фильма</h1>
+        <h1 class="modal-title fs-5" id="modalFilmInfo">О фильме</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -126,7 +126,6 @@
     </div>
   </div>
 </div>
-
 @endsection
 
 @section('custom-script')
