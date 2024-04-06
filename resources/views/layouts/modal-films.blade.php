31d823f474cde6783ffb958897372fbad27a956d
andrey-nee <andrey.nee91@gmail.com>
2024-04-06 12:58:32 +0500

    feat: add film-modal temporary content

---
+++ resources/views/layouts/modal-films.blade.php
@@ -0,0 +1,29 @@
+<div class="table-responsive small py-3 films">
+  @csrf
+  <table class="table table-hover table-bordered table-striped table-sm table-films">
+
+    <thead class="table-films__head">
+      <tr class="table-films__row">
+        {{-- <th scope="col" class="table-films__header">Обложка</th> --}}
+        <th scope="col" class="table-films__header">Название</th>
+        <th scope="col" class="table-films__header">Описание</th>
+        <th scope="col" class="table-films__header">Жанр</th>
+        <th scope="col" class="table-films__header">Год</th>
+        <th scope="col" class="table-films__header">Режиссер</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr class="table-films__row" data-bs-toggle="modal" data-bs-target="#filmInfo" data-id="{{ $data->id }}">
+        {{-- <td class="table-films__data table-films__film-image"><img src="/images/{{ $image }}"
+            alt="{{ $data->name }}"></td> --}}
+        <td class="table-films__data">{{ $data->name_ru }}</td>
+        <td class="table-films__data">{{ $data->description }}</td>
+        <td class="table-films__data">{{ $data->genre_id }}</td>
+        <td class="table-films__data">{{ $data->year }}</td>
+        <td class="table-films__data">{{ $data->director_id }}</td>
+      </tr>
+    </tbody>
+
+  </table>
+</div>
