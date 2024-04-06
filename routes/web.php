4b713915830740c5255087399458bc682ab18d89
andrey-nee <andrey.nee91@gmail.com>
2024-04-06 12:45:42 +0500

    feat: add filmsInfoContent route for film info modal

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ use App\Http\Controllers\FilmController;
 
 Route::get('/', [CollectionController::class, 'welcome'])->name('welcome');
 Route::get('/films', [FilmController::class, 'index'])->name('films');
+
+Route::post('/films/info', [FilmController::class, 'info'])->name('filmsInfoContent');
 // Route::get('/{page}', [CollectionController::class, 'loadPage'])->name('load.page');
 
 Auth::routes();
