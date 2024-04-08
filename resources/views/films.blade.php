cb4815bce38a39b3545d3f1b816ef955bfc9343a
andrey-nee <andrey.nee91@gmail.com>
2024-04-08 12:02:50 +0500

    fix: update input id for form-new-collection

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -75,34 +75,34 @@
       <input type="text" class="form-control" id="collection-name-ru" required>
     </div>
     <div class="mb-3">
-      <label for="collection-name-ru" class="col-form-label">
+      <label for="collection-description" class="col-form-label">
         Описание<span class="required"></span>
       </label>
-      <input type="text" class="form-control" id="collection-name-ru" required>
+      <input type="text" class="form-control" id="collection-description" required>
     </div>
     <div class="mb-3">
-      <label for="collection-name-ru" class="col-form-label">
+      <label for="collection-genre_id" class="col-form-label">
         Жанр<span class="required"></span>
       </label>
-      <input type="text" class="form-control" id="collection-name-ru" required>
+      <input type="text" class="form-control" id="collection-genre_id" required>
     </div>
     <div class="mb-3">
-      <label for="collection-name-ru" class="col-form-label">
+      <label for="collection-year" class="col-form-label">
         Год<span class="required"></span>
       </label>
-      <input type="text" class="form-control" id="collection-name-ru" required>
+      <input type="text" class="form-control" id="collection-year" required>
     </div>
     <div class="mb-3">
-      <label for="collection-name-ru" class="col-form-label">
+      <label for="collection-director_id" class="col-form-label">
         Режиссер<span class="required"></span>
       </label>
-      <input type="text" class="form-control" id="collection-name-ru" required>
+      <input type="text" class="form-control" id="collection-director_id" required>
     </div>
     <div class="mb-3">
-      <label for="collection-name-ru" class="col-form-label">
+      <label for="collection-image" class="col-form-label">
         Обложка<span class="required"></span>
       </label>
-      <input type="text" class="form-control" id="collection-name-ru" required>
+      <input type="text" class="form-control" id="collection-image" required>
     </div>
   </form>
 @endsection
