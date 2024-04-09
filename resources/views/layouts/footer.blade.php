484a7a4ac76b7cb863632134da110dea5f00ec14
andrey-nee <andrey.nee91@gmail.com>
2024-04-09 14:06:18 +0500

    fix: change route for collection.index & film.index

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <ul class="footer-nav nav justify-content-center border-bottom pb-3 mb-3">
-  <li class="footer-nav__item nav-item"><a href="{{ route('welcome') }}" class="nav-link px-2 text-body-secondary"
+  <li class="footer-nav__item nav-item"><a href="{{ route('welcome.index') }}" class="nav-link px-2 text-body-secondary"
       data-page="welcome">Home</a></li>
-  <li class="footer-nav__item nav-item"><a href="{{ route('films') }}" class="nav-link px-2 text-body-secondary"
+  <li class="footer-nav__item nav-item"><a href="{{ route('films.index') }}" class="nav-link px-2 text-body-secondary"
       data-page="films">Films</a></li>
 </ul>
 <p class="footer-info text-center text-body-secondary">© 2024 Company, Inc</p>
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use App\Http\Controllers\FilmController;
 |
 */
 
-Route::get('/', [CollectionController::class, 'welcome'])->name('welcome');
-Route::get('/films', [FilmController::class, 'index'])->name('films');
+Route::get('/', [CollectionController::class, 'index'])->name('welcome.index');
+// Route::get('/films', [FilmController::class, 'index'])->name('films');
 
 Route::post('/films/info', [FilmController::class, 'info'])->name('filmsInfoContent');
 // Route::get('/{page}', [CollectionController::class, 'loadPage'])->name('load.page');
