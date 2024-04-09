465ffebf5e8e15647b567691fdd4091102ae7461
andrey-nee <andrey.nee91@gmail.com>
2024-04-09 14:10:42 +0500

    fix: replace films table to films-data.blade.php

--- resources/views/sections/films/films.blade.php
+++ resources/views/sections/films/films.blade.php
@@ -23,29 +23,7 @@
         </tr>
       </thead>
       <tbody>
-
-        @foreach ($films as $film)
-          @php
-            $image = '';
-            if (count($film->images) > 0) {
-                $image = $film->images[0]['image'];
-            } else {
-                $image = 'no_image.jpg';
-            }
-          @endphp
-
-          <tr class="table-films__row" data-bs-toggle="modal" data-bs-target="#filmInfo" data-id="{{ $film->id }}">
-            <td class="table-films__data">{{ $loop->iteration }}</td> {{-- $loop->iteration (встроено в Laravel) считает кол-во итераций внутри @foreach --}}
-            <td class="table-films__data table-films__film-image"><img src="/images/{{ $image }}"
-                alt="{{ $film->name }}"></td>
-            <td class="table-films__data">{{ $film->name_ru }}</td>
-            <td class="table-films__data">{{ $film->description }}</td>
-            <td class="table-films__data">{{ $film->genre_id }}</td>
-            <td class="table-films__data">{{ $film->year }}</td>
-            <td class="table-films__data">{{ $film->director_id }}</td>
-          </tr>
-        @endforeach
-
+        @include('ajax.films-data')
       </tbody>
     </table>
 
