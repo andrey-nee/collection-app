ed8d45d8e555a3473f8a6b6177348783f3fb09b2
andrey-nee <andrey.nee91@gmail.com>
2024-04-15 18:39:29 +0500

    fix: add actual genre and director for film-info-modal

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -24,19 +24,31 @@ class FilmController extends Controller
   public function info(Request $request)
   {
     $id = $request->id;
+
     $data = Film::with('images')->findOrFail($id);
+
+    // Получаем картинку для фильма
     $image = $data->images->isEmpty()
       ? 'no_image.jpg'
       : $data->images->first()->image;
-    return view('ajax.films-info-content', compact('data', 'image'));
+
+    // Получаем жанр фильма
+    $genre = $data->genre->name;
+
+    // Получаем режиссера фильма
+    $director = $data->director->name;
+
+    return view('ajax.films-info-content', compact('data', 'image', 'genre', 'director'));
   }
 
-  public function genre() {
+  public function genre()
+  {
     $films = Film::with('genre')->get();
     return view('sections.films.films', compact('genre'));
   }
 
-  public function director() {
+  public function director()
+  {
     $film = Film::with('director')->get();
     return view('section.films.films', compact('director'));
   }
--- resources/views/ajax/films-info-content.blade.php
+++ resources/views/ajax/films-info-content.blade.php
@@ -2,9 +2,9 @@
   <h2 class="modal-title fs-5">{{ $data->name_ru }}</h2>
   <div class="d-inline-flex gap-3">
     <p class="card-subtitle fs-6">Год: <span>{{ $data->year }}</span></p>
-    <p class="card-subtitle fs-6">Жанр: <span>{{ $data->genre_id }}</span></p>
+    <p class="card-subtitle fs-6">Жанр: <span>{{ $genre }}</span></p>
   </div>
-  <p class="card-subtitle fs-6">Режиссер: <span>{{ $data->director_id }}</span></p>
+  <p class="card-subtitle fs-6">Режиссер: <span>{{ $director }}</span></p>
   <div class="modal-film-info__description">
     <p class="card-subtitle fs-6">Описание:</p>
     <div class="row">
