e30535a3c40bdd2e331b9355e2206b7e275b36d8
andrey-nee <andrey.nee91@gmail.com>
2024-04-15 16:36:19 +0500

    fix: change film info modal style

--- resources/views/ajax/films-info-content.blade.php
+++ resources/views/ajax/films-info-content.blade.php
@@ -1,13 +1,19 @@
-<h2 class="modal-title fs-5">{{ $data->name_ru }}</h2>
-<p class="card-subtitle">Год: <span>{{ $data->year }}</span></p>
-<p class="card-subtitle">Жанр: <span>{{ $data->genre_id }}</span></p>
-<p class="card-subtitle">Режиссер: <span>{{ $data->director_id }}</span></p>
-<div class="row">
-  <div class="col-8">
-    <p class="card-subtitle">Описание:</p>
-    <p>{{ $data->description }}</p>
+<div class="modal-film-info">
+  <h2 class="modal-title fs-5">{{ $data->name_ru }}</h2>
+  <div class="d-inline-flex gap-3">
+    <p class="card-subtitle fs-6">Год: <span>{{ $data->year }}</span></p>
+    <p class="card-subtitle fs-6">Жанр: <span>{{ $data->genre_id }}</span></p>
   </div>
-  <div class="col-4" id="filmImageContainer">
-    <img id="filmImage" src="/images/{{ $image }}" alt="{{ $data->name_ru }}">
+  <p class="card-subtitle fs-6">Режиссер: <span>{{ $data->director_id }}</span></p>
+  <div class="modal-film-info__description">
+    <p class="card-subtitle fs-6">Описание:</p>
+    <div class="row">
+      <div class="col-8">
+        <p>{{ $data->description }}</p>
+      </div>
+      <div class="col-4 modal-film-info__image">
+        <img src="/images/{{ $image }}" alt="{{ $data->name_ru }}">
+      </div>
+    </div>
   </div>
 </div>
