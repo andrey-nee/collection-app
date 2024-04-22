7622ccec655fab113cfbce1b1d3fade937a7172d
andrey-nee <andrey.nee91@gmail.com>
2024-04-22 17:10:25 +0500

    feat: add Select2 library

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -10,4 +10,4 @@
 {{-- Для этого просто раскомментируйте следующую строчку: --}}
 <script src="{{ asset('jquery-3.7.1.min.js') }}"></script>
 
-@vite(['resources/css/bootstrap.min.css', 'resources/css/datatables.min.css', 'resources/sass/app.scss', 'resources/js/bootstrap.bundle.min.js', 'resources/js/datatables.min.js', 'resources/js/app.js'])
+@vite(['resources/css/bootstrap.min.css', 'resources/css/datatables.min.css', 'resources/css/select2.min.css', 'resources/sass/app.scss', 'resources/js/bootstrap.bundle.min.js', 'resources/js/datatables.min.js', 'resources/js/select2.full.min.js', 'resources/js/app.js'])
