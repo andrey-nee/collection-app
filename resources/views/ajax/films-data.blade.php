4cff8fe283785c9431d2d8138434e70d78a11288
andrey-nee <andrey.nee91@gmail.com>
2024-04-22 14:18:09 +0500

    fix: update TableFilms row counter

--- resources/views/ajax/films-data.blade.php
+++ resources/views/ajax/films-data.blade.php
@@ -9,9 +9,8 @@
   @endphp
 
   <tr class="table-films__row" data-bs-toggle="modal" data-bs-target="#filmInfo" data-id="{{ $film->id }}">
-    <td class="table-films__data">{{ $films->firstItem() + $loop->index }}</td>
-    {{-- ->firstItem() - это порядковый номер первого элемента на текущей странице (встроено в Laravel) --}}
-    {{-- $loop->index - это индекс текущей итерации цикла @foreach (встроено в Laravel) --}}
+    <td class="table-films__data">{{$loop->iteration }}</td>
+    {{-- $loop->iteration - это текущая итерация цикла (начинается с 1) цикла @foreach (встроено в Laravel) --}}
     <td class="table-films__data table-films__film-image"><img src="/images/{{ $image }}" alt="{{ $film->name }}">
     </td>
     <td class="table-films__data">{{ $film->name_ru }}</td>
